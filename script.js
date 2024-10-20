1f53d5ddbd00120dc1cd4e196d69e0f129e608f9
LinXBalajiN <balajiit062@gmail.com>
2024-10-20 17:21:49 +0530

    style changes and sidebar works done

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-let sideNavbar = document.getElementsByClassName("side-navbar");
-sideNavbar[0].style.display = "none";
+// sidebar toggle
+let sideNavbarToggle = document.querySelector(".navbar-menu-toggle");
+let sidebar = document.querySelector(".side-navbar");
+let close = document.querySelector('.close');
+sideNavbarToggle.addEventListener('click', function(){
+    sidebar.style.display === 'block' ? sidebar.style.display = 'none' : sidebar.style.display = 'block';
+});
+close.addEventListener('click', function(){
+    sidebar.style.display = 'none';
+});
